09210239037686eff5cf81ba29c3a757ff6171b0
Chikwendu Chibudom <chibudomchikwendu@gmail.com>
2023-09-04 01:22:49 +0100

    New Modifications

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css'; // Import your CSS file
 import { useEffect, useState } from "react";
+
 export default function App() {
   const [advice, setAdvice] = useState('');
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
   async function getAdvice() {
     const res = await fetch('https://api.adviceslip.com/advice');
     const data = await res.json();
@@ -16,9 +18,9 @@ export default function App() {
   }, []);
 
   return (
-    <div>
-      <h1>{advice}</h1>
-      <button onClick={getAdvice}>Get Advice</button>
+    <div className="advice-container">
+      <h1 className="advice-heading">{advice}</h1>
+      <button onClick={getAdvice} className="get-advice-button">Get Advice</button>
       <Message count={count} />
     </div>
   );
@@ -26,8 +28,8 @@ export default function App() {
 
 function Message(props) {
   return (
-    <p>
+    <p className="message-paragraph">
       Advice #<strong>{props.count}</strong>
     </p>
-  )
+  );
 }
